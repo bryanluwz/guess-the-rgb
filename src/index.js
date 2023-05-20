82edc157f6698115f516885d34c58cf2456b47c7
Bryan <bryanlu.my@gmail.com>
2023-05-20 14:42:00 +0800

    skeleton structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-	<HashRouter basename={process.env.PUBLIC_URL}>
+	<HashRouter>
 		<App />
 	</HashRouter>
 );
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,12 +1,39 @@
 import { Component } from "react";
 import { ContentDisplay } from "../components/others/";
-import { Error404Page } from "../components/pages";
 
 export default class Main extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentRGBValue: null,
+			currentCSSColor: null
+		};
+	}
+
+	componentDidMount() {
+		const { r, g, b } = this.createRandomRGB();
+		this.setState({
+			currentRGBValue: { r: r, g: g, b: b },
+			currentCSSColor: this.createCSSColorStringFromRGBValue(r, g, b)
+		});
+	}
+
+	createRandomRGB = () => {
+		const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+		const r = randomBetween(0, 255);
+		const g = randomBetween(0, 255);
+		const b = randomBetween(0, 255);
+		return { r, g, b };
+	};
+
+	createCSSColorStringFromRGBValue = (r, g, b) => {
+		return `rgb(${r},${g},${b})`;
+	};
+
 	render() {
 		return (
 			<ContentDisplay
-				backButtonRoute={"https://bryanluwz.github.io/"}
+				backButtonRedirect={"https://bryanluwz.github.io/#/fun-stuff"}
 				displayName={Main.displayName}
 				displayClearHistory={false}
 				faIcon={"fa-trash"}
@@ -14,10 +41,42 @@ export default class Main extends Component {
 				router={this.props.router}
 				handleHeaderTitleClick={() => { console.log("please do not the cat"); }}
 			>
-				<Error404Page customWarning={"well... this is awkward..."} />
+				<div className="gtr-wrapper">
+					<div className="gtr-container">
+						<div className="gtr-display-container">
+							<div
+								style={{ backgroundColor: this.state.currentCSSColor }}
+								className="gtr-display">
+							</div>
+						</div>
+						<div className="gtr-ui-container">
+							<div className="gtr-ui-buttons">
+								<button>new game</button>
+								<button>change input method</button>
+							</div>
+							<div className="gtr-ui-input-container">
+								<div className="gtr-ui-input" >
+									<i className="fa fa-sort-up" />
+									<input />
+									<i className="fa fa-sort-down" />
+								</div>
+								<div className="gtr-ui-input" >
+									<i className="fa fa-sort-up" />
+									<input />
+									<i className="fa fa-sort-down" />
+								</div>
+								<div className="gtr-ui-input" >
+									<i className="fa fa-sort-up" />
+									<input />
+									<i className="fa fa-sort-down" />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</ContentDisplay>
 		);
 	}
 }
 
-Main.displayName = "Display Name";
+Main.displayName = "Guess the RGB";
